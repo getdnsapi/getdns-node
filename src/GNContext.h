09e5d7641178837890b02c254355f8acf397b5b7
Joel Purra <code+github@joelpurra.com>
2017-03-23 01:51:02 +0100

    Update copyright years

--- src/GNContext.h
+++ src/GNContext.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, Verisign, Inc.
+ * Copyright (c) 2014, 2015, 2016, 2017, Verisign, Inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
